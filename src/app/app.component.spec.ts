1907ef87f4835c218a33efc5cfacb8496d8587df
David Calderon <dcalderonlo@outlook.com>
2025-04-18 20:33:01 -0400

    Rename project references from 'the_meal-restaurant' to 'the_meal_recipes' in configuration and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'the_meal-restaurant' title`, () => {
+  it(`should have the 'the_meal_recipes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('the_meal-restaurant');
+    expect(app.title).toEqual('the_meal_recipes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, the_meal-restaurant');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, the_meal_recipes');
   });
 });
